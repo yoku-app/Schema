2cd47f8d965a5323768fb208dc3e0f58529e3c75
Jared Tucker <jtuc0006@student.monash.edu>
2025-01-11 20:03:19 +1100

    Update Naming Conventions

--- dist/profile.schema.d.ts
+++ dist/profile.schema.d.ts
@@ -5,7 +5,7 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export interface ProfileSchema {
+export interface UserProfile {
   id: number;
   userId: string;
   email: string;
