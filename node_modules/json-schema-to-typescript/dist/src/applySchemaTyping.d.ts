63c46720aa270051b0be9b1a03e4fd39c2b65a36
Jared Tucker <jtuc0006@student.monash.edu>
2025-01-11 18:45:39 +1100

    Fixed Ruh roh

--- node_modules/json-schema-to-typescript/dist/src/applySchemaTyping.d.ts
+++
@@ -1,2 +0,0 @@
-import type { LinkedJSONSchema } from './types/JSONSchema';
-export declare function applySchemaTyping(schema: LinkedJSONSchema): void;
--- node_modules/json-schema-to-typescript/src/formatter.ts
+++
@@ -1,9 +0,0 @@
-import {format as prettify} from 'prettier'
-import {Options} from './'
-
-export async function format(code: string, options: Options): Promise<string> {
-  if (!options.format) {
-    return code
-  }
-  return prettify(code, {parser: 'typescript', ...options.style})
-}
